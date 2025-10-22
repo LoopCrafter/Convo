b072788f63a710b9fcfb68494afe48fbe91d184e
Hamed <ostovari.eng@gmail.com>
2025-10-22 21:00:15 +0200

    Implemented User Model

---
+++ backend/src/models/User.model.ts
@@ -0,0 +1,60 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      uired: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    avatarUrl: {
+      type: String,
+      default: "",
+    },
+    isOnboarded: {
+      type: Boolean,
+      default: false,
+    },
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const User = model("User", userSchema);
+//pre hook to hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+export default User;
