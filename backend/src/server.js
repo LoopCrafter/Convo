d0624096146cf4f5c04a5ab922eb27a5edaa1b14
Hamed <ostovari.eng@gmail.com>
2025-10-22 20:41:45 +0200

    Connected to DB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import router from "./routers/index.js";
+import { connectDB } from "../lib/db.js";
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
@@ -10,4 +11,5 @@ app.use("/api/v1", router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
 });
